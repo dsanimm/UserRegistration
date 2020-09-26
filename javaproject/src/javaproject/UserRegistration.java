99c521c3a887e1872152b9a07439dd6b670cd962
dsanimm <dsanimm@gmail.com>
2020-09-26 17:23:50 +0530

    UC2

--- javaproject/src/javaproject/UserRegistration.java
+++ javaproject/src/javaproject/UserRegistration.java
@@ -5,22 +5,32 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
+	
+	public static void check(String toCheck,String regex){
+		Pattern p = Pattern.compile(regex);
+		
+		Matcher m = p.matcher(toCheck);
+		
+		if(m.find()) {
+			System.out.println("Valid");
+		}
+		
+		else {
+			System.out.println("Invalid");
+		}
+	}
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter First Name:");
 		String fname = sc.next();
 		String regex = "(^[A-Z][a-z]{2,})";
-		Pattern p = Pattern.compile(regex);
+		check(fname,regex);
+		System.out.println("Enter Last Name:");
+		String lname = sc.next();
+		check(lname,regex);
 		
-		Matcher m = p.matcher(fname);
 		
-		if(m.find()) {
-			System.out.println("First Name is Valid");
-		}
 		
-		else {
-			System.out.println("Invalid First Name");
-		}
 		
 		
 	}
